b733c1ddc268021bab904223f3833559f9c62ef2
Manglam <64706997+Mangalofficial@users.noreply.github.com>
2020-06-23 03:54:23 +0530

    port

--- read.py
+++ read.py
@@ -1,4 +1,4 @@
-open_file=open('/var/lib/jenkins/workspace/My_Job2/port.py','r')
+open_file=open('/jenkins/workspace/JOB2_task4/port.py','r')
 read=open_file.read()
 for a in read:
   if a.isnumeric() == True: 
